7334ddbc9580359dae023eace70855b98c05da8e
John <jddhalogamer@gmail.com>
2021-10-13 20:47:06 -0500

    minor styling change.

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div>
+        <div id="footer">
             <footer>
                 <h4>Property of J.D. Industries</h4>
                 <p>Trademarked 2021</p>
