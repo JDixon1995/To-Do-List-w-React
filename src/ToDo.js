02ffa5d0ec09ed6045bb290208a7acdf04667d4c
John <jddhalogamer@gmail.com>
2021-10-23 19:45:14 -0500

    cleaned up U/I.

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { FaTimes } from 'react-icons/fa';
 
 const ToDo = ( {todo} ) => {
     return (
         <div className="to-do">
-          <h4>{todo.content}</h4>
+          <h4>{todo.content}
+          <FaTimes style={{ color: 'red' }} />
+          </h4>
           <p>{todo.dueDate}</p>  
         </div>
     )
